ba7f79951b28b81fa2474faaf7454d8f94962c9b
Alexey Belyashov <alexey.belyashov@vseinstrumenti.ru>
2018-03-29 22:52:29 +0300

    Hotfix

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "app" {
-  count = 2
+  count = "${var.count}"
 
   name         = "reddit-app${count.index}"
   machine_type = "g1-small"
